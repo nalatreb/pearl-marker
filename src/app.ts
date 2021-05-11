58093fbf8963eae7724f0ba86d7a576811890d99
bertalanV <bertalan.verebelyi@gmail.com>
2021-05-11 20:25:56 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import { Alma } from './test';
+
+const kukac: String = 'ALMA';
+
+const test: String = 'KORTE';
+
+for (let index = 0; index < 5; index++) {
+    console.log(index);
+}
+console.log(kukac);
+console.log(test);
+
+let alma = new Alma('aaaaa');
+
+console.log(alma.alma);
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export class Alma {
+    constructor (public alma: String) {}
+}
